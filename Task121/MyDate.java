aa2c615ec72c544d536b1fb91eba7819b8570f97
monicamagcuro21 <monixmagcuro@gmail.com>
2025-10-24 20:32:28 +0800

    TaskSheet1-2-2

--- Task121/MyDate.java
+++ Task121/MyDate.java
@@ -1,18 +1,20 @@
 package Task121;
+
 public class MyDate {
     public static void main(String[] args) {
     
-        
+       
         DateTask date1 = new DateTask(1,1,1987);
         DateTask date2 = new DateTask(9,21,1984);
-        DateTask leap = new DateTask(9,21,1984);
-
+        
+       
        
 
         //System.out.println(date1.toString());
+        System.out.println(date1.toString());
         System.out.println(date2.toString());
        // display leapyears
-       System.out.println(leap.leapYears());
-    
+       
+        DateTask.leapYears();
     }
 }
