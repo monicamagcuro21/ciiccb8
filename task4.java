34ec539d374df307d1d7cf5e57e08f14d6560fe7
monicamagcuro21 <monixmagcuro@gmail.com>
2025-11-22 08:02:20 +0800

    Task4

---
+++ task4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class task4 {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String words = in.nextLine().toLowerCase();
+
+        StringBuilder sb = new StringBuilder(words);
+        String rev = sb.reverse().toString().toLowerCase();
+
+      
+
+       if(words.equals(rev) == true){
+        System.out.println("This word is a palindrome");
+       }else{
+        System.out.println("This is not a palindrome");
+       }
+
+      
+        
+
+        
+        
+
+
+        
+    }
+}
